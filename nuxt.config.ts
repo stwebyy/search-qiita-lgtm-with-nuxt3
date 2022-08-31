ac8c6456de820571bd5a2ce0f0a843febd63a704
stwebyy <yuya.sato4@persol.co.jp>
2022-09-01 00:32:31 +0900

    feat: vuetify3をnuxtに読み込ませ、btnの表示

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,8 @@ import { defineNuxtConfig } from 'nuxt';
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   srcDir: 'src',
+  css: ['vuetify/lib/styles/main.sass', 'mdi/css/materialdesignicons.min.css'],
+  build: {
+    transpile: ['vuetify'],
+  },
 });
